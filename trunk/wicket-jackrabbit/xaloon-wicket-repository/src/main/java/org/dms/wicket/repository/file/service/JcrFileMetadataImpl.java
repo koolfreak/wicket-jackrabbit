e1b8fed9eb3baac5d09e2efe28739b5df466d02b
eman.nollase <eman.nollase@49cd5a18-9e1b-11de-8f2f-915425aaff72>
2009-09-19 12:05:50 +0000

    re-organize/refactor

---
+++ trunk/wicket-jackrabbit/xaloon-wicket-repository/src/main/java/org/dms/wicket/repository/file/service/JcrFileMetadataImpl.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package org.dms.wicket.repository.file.service;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.dms.wicket.repository.db.dao.JcrFileStorageDao;
+import org.dms.wicket.repository.db.model.FileDescription;
+import org.dms.wicket.repository.db.model.FileVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.xaloon.wicket.component.exception.FileStorageException;
+import org.xaloon.wicket.component.repository.FileRepository;
+
+/**
+ * @author Emmanuel Nollase - emanux
+ * created 2009 9 19 - 01:11:07
+ */
+@Component
+public class JcrFileMetadataImpl implements JcrFileMetadata
+{
+
+    @Autowired private FileRepository fileRepository;
+    @Autowired private JcrFileStorageDao jcrFileStorageDao;
+    /* (non-Javadoc)
+     * @see org.dms.wicket.repository.db.service.JcrFileMetadata#storeFileVersion(java.lang.String, java.lang.String, java.lang.String, java.io.InputStream)
+     */
+    public void storeFileVersion(String path, String name, String mimeType,
+	    InputStream input) throws FileStorageException
+    {
+	final FileDescription file = fileRepository.storeFileVersion(path, name, mimeType, input);
+	if(file != null)
+	{
+	    jcrFileStorageDao.save(file);
+	}
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.dms.wicket.repository.db.service.JcrFileMetadata#storeNextVersion(java.lang.String, java.io.InputStream)
+     */
+    public void storeNextVersion(FileDescription file, InputStream fileStream)
+	    throws FileStorageException
+    {
+	fileRepository.storeNextVersion(file, fileStream);
+	jcrFileStorageDao.update(file);
+    }
+
+    public List<FileVersion> getFileVersions(String path)
+	    throws FileStorageException
+    {
+	return fileRepository.getFileVersions(path);
+    }
+
+    public void restoreVersion(FileDescription file, String verName)
+	    throws FileStorageException
+    {
+	fileRepository.restoreVersion(file, verName);
+	jcrFileStorageDao.update(file);
+    }
+ }
