42945d6a2883cc83112e63d07ff3de0b355b0e60
eman.nollase <eman.nollase@49cd5a18-9e1b-11de-8f2f-915425aaff72>
2009-10-28 06:16:05 +0000

    make thread safe the jackrabbit client

--- wicket-jackrabbit/dms-jackrabbit-remote/src/main/java/org/dms/wicket/WicketApplication.java
+++ wicket-jackrabbit/dms-jackrabbit-remote/src/main/java/org/dms/wicket/WicketApplication.java
@@ -2,11 +2,18 @@ package org.dms.wicket;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.wicket.Request;
+import org.apache.wicket.RequestCycle;
+import org.apache.wicket.Response;
 import org.apache.wicket.extensions.ajax.markup.html.form.upload.UploadWebRequest;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.protocol.http.WebRequest;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+import org.dms.wicket.component.ContentWebRequestCycle;
+import org.dms.wicket.component.ThreadLocalSessionFactory;
 import org.dms.wicket.page.HomePage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  * Application object for your web application. If you want to run this
@@ -62,6 +69,27 @@ public class WicketApplication extends WebApplication
 	return (WicketApplication) WebApplication.get();
     }
     
+    
+    @Override
+    public RequestCycle newRequestCycle(Request request, Response response)
+    {
+	return new ContentWebRequestCycle(this, (WebRequest) request, response)
+	{
+	    @Override
+	    protected ThreadLocalSessionFactory getContentSessionFactory()
+	    {
+		return getThreadLocalSessionFactory();
+	    }
+	};
+    }
+
+    
+    public final ThreadLocalSessionFactory getThreadLocalSessionFactory()
+    {
+	ApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+	return (ThreadLocalSessionFactory) ctx.getBean("jcrThreadLocalSessionFactory");
+    }
+
     public String getDmsRepoPath()
     {
 	return DMS_REPO_PATH;
